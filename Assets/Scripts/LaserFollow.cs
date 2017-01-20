02c5765f0ebf6c1d8f1f247a10be98ba673d2b6b
Jacob Cheatley <jacobcheatley@gmail.com>
2017-01-21 03:53:03 +1300

    Laser of death follows player

---
+++ Assets/Scripts/LaserFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserFollow : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private float minDistance = 10f;
+    [SerializeField]
+    private float speed = 5f;
+
+	void Update()
+    {
+        if (player.transform.position.x - transform.position.x > minDistance)
+            transform.position = new Vector3(player.transform.position.x - minDistance + 0.5f, player.transform.position.y);
+
+        transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, player.transform.position.y);
+	}
+}
