48f948698f51be940e3050365ae3feb6d82f4da8
Jacob Cheatley <jacobcheatley@gmail.com>
2017-01-21 09:35:37 +1300

    Player touches laser, and laser visuals fixed

---
+++ Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+class PlayerDeath : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Laser")
+        {
+            Debug.Log("Zap"); // Do restart logic here
+        }
+    }
+}
