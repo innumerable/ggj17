1759ca1dd04ea67c5d5cec46291d0c294dde0cba
Jacob Cheatley <jacobcheatley@gmail.com>
2017-01-21 10:00:53 +1300

    Laser sfx when close

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -11,6 +11,13 @@ public class Laser : MonoBehaviour
     [SerializeField]
     private float speed = 5f;
 
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
 	void Update()
     {
         if (player.transform.position.x - transform.position.x > minDistance)
@@ -19,5 +26,6 @@ public class Laser : MonoBehaviour
         transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, player.transform.position.y);
 
         Camera.main.backgroundColor = Color.Lerp(new Color(0.5f, 0f, 0f, 1f), Color.black, (player.transform.position.x - transform.position.x) / (minDistance / 3f));
+        audioSource.volume = Mathf.Lerp(1, 0, (player.transform.position.x - transform.position.x) / (minDistance / 1.5f));
     }
 }
