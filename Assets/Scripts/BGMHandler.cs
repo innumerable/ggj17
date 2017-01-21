c6f1964c65b6ba80732369d85307c24f7a87de6c
Daniel Oosterwijk <danieloosterwijk@gmail.com>
2017-01-21 16:23:57 +1300

    Fuck git

---
+++ Assets/Scripts/BGMHandler.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class BGMHandler : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody2D player;
+
+    [SerializeField]
+    AudioSource[] sources;
+    [SerializeField]
+    AudioMixer[] mixers;
+
+    [SerializeField]
+    float[] defaultVolumes = { 1, 0, 0 };
+
+    [SerializeField]
+    float fadeTime = 1f;
+
+    [SerializeField]
+    [Range(0.5f, 1.5f)]
+    float minPitch = 0.75f;
+
+    [SerializeField]
+    [Range(0.5f, 1.5f)]
+    float maxPitch = 1.25f;
+
+    [SerializeField]
+    float layer2VelocityLowerThreshold = 30;
+
+    [SerializeField]
+    float layer3VelocityLowerThreshold = 60;
+
+    string[] exposedParameterNames =
+    {
+        "PitchLayer1",
+        "PitchLayer2",
+        "PitchLayer3",
+    };
+
+    // Use this for initialization
+    void Start ()
+    {
+        for (int i = 0; i < sources.Length; i++)
+        {
+            sources[i].volume = defaultVolumes[i];
+            sources[i].loop = true;
+        }
+	}
+
+    void Update()
+    {
+        if (player.velocity.magnitude > layer2VelocityLowerThreshold)
+        {
+            sources[1].volume = Mathf.Lerp(sources[1].volume, 1, Time.deltaTime * fadeTime);
+
+
+        }
+        else
+        {
+            sources[1].volume = Mathf.Lerp(sources[1].volume, 0, Time.deltaTime * fadeTime);
+
+
+        }
+
+        if (player.velocity.magnitude > layer3VelocityLowerThreshold)
+        {
+            sources[2].volume = Mathf.Lerp(sources[1].volume, 1, Time.deltaTime * fadeTime);
+        }
+        else
+        {
+            sources[2].volume = Mathf.Lerp(sources[1].volume, 0, Time.deltaTime * fadeTime);
+        }
+    }
+}
