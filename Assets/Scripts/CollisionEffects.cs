efd667deae5e61281c9277c86792221840e16b5f
Jacob Cheatley <jacobcheatley@gmail.com>
2017-01-21 14:43:36 +1300

    Camera dynamically adjusts to ground distance and velocity

--- Assets/Scripts/CollisionEffects.cs
+++ Assets/Scripts/CollisionEffects.cs
@@ -4,22 +4,5 @@ using UnityEngine;
 
 public class CollisionEffects : MonoBehaviour
 {
-    [SerializeField]
-    private float thresholdVelocity;
-
-    private ScreenShake screenShake;
-    
-	void Start()
-	{
-	    screenShake = Camera.main.gameObject.GetComponent<ScreenShake>();
-	}
-	
-	void OnCollisionEnter2D(Collision2D collision)
-	{
-	    float vel = collision.relativeVelocity.magnitude;
-        if (vel > thresholdVelocity)
-        {
-		    screenShake.StartShake(0.1f, vel/30f);
-        }
-    }
+    // There will be stuff here
 }
