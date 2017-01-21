f698959851fd814bf78b2d07eedbc7ecf421098f
Jacob Cheatley <jacobcheatley@gmail.com>
2017-01-21 18:44:47 +1300

    Laser follow improved

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -7,25 +7,36 @@ public class Laser : MonoBehaviour
     [SerializeField]
     private GameObject player;
     [SerializeField]
-    private float minDistance = 10f;
+    private float thresholdDistance = 48f;
     [SerializeField]
-    private float speed = 5f;
-
+    private float minSpeed = 6f;
+    
     private AudioSource audioSource;
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        StartCoroutine(PlaceLater());
+    }
+
+    IEnumerator PlaceLater()
+    {
+        transform.position = player.transform.position + Vector3.left * thresholdDistance * 6;
+        yield return new WaitForSeconds(0.1f);
+        transform.position = player.transform.position + Vector3.left * thresholdDistance * 6;
     }
 
 	void Update()
-    {
-        if (player.transform.position.x - transform.position.x > minDistance)
-            transform.position = new Vector3(player.transform.position.x - minDistance + 0.5f, player.transform.position.y);
+	{
+	    float distance = player.transform.position.x - transform.position.x;
+	    float speed = distance < thresholdDistance ? minSpeed : Mathf.LerpUnclamped(minSpeed, minSpeed * 2f, distance - thresholdDistance);
+//	    float speed = minSpeed;
+        print(speed);
 
         transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, player.transform.position.y);
 
-        Camera.main.backgroundColor = Color.Lerp(new Color(0.5f, 0f, 0f, 1f), Color.black, (player.transform.position.x - transform.position.x) / (minDistance / 3f));
-        audioSource.volume = Mathf.Lerp(1, 0, (player.transform.position.x - transform.position.x) / (minDistance / 1.5f));
+        // Visuals and audio
+        Camera.main.backgroundColor = Color.Lerp(new Color(0.5f, 0f, 0f, 1f), Color.black, (player.transform.position.x - transform.position.x) / (thresholdDistance / 3f));
+        audioSource.volume = Mathf.Lerp(1, 0, (player.transform.position.x - transform.position.x) / (thresholdDistance / 1.5f));
     }
 }
