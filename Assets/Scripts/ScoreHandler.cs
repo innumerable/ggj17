b0d895969b6a0b062f58c73fb33048afaf027a21
Daniel Oosterwijk <danieloosterwijk@gmail.com>
2017-01-21 15:53:03 +1300

    Git is dumb and didn't push my ScoreHandler

---
+++ Assets/Scripts/ScoreHandler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreHandler : MonoBehaviour
+{
+    [SerializeField]
+    Text scoreTextUI;
+
+	[SerializeField]
+    float scorePerUnitDistance = 1000;
+
+    long score;
+
+    float originalPositionX;
+
+    void Start()
+    {
+        originalPositionX = transform.position.x;
+    }
+    
+	void Update ()
+    {
+        float distanceMoved = transform.position.x - originalPositionX;
+        score = (int)Mathf.Max(distanceMoved * scorePerUnitDistance, score);
+        scoreTextUI.text = string.Format("Score: {0}", score);
+        
+	}
+}
